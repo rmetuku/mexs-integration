35c138e54b264283336b43205b907b796431cd57
Raghuramulum <Raghuramulum@digitalinfuzion.com>
2018-09-05 19:26:41 +0530

    Addded update and cancel appointment capabilities

--- src/main/java/com/nousinfo/mexsintr/MexsIntegrationApplication.java
+++ src/main/java/com/nousinfo/mexsintr/MexsIntegrationApplication.java
@@ -3,7 +3,6 @@ package com.nousinfo.mexsintr;
 import static com.google.common.base.Predicates.or;
 import static springfox.documentation.builders.PathSelectors.regex;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
---
+++ src/main/java/com/nousinfo/mexsintr/bo/AppointmentUpdateBO.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.nousinfo.mexsintr.bo;
+
+/**
+ * @author raghuramulum
+ *
+ */
+public class AppointmentUpdateBO extends AppointmentBO {
+
+	private String meetingId;
+
+	/**
+	 * @return the meetingId
+	 */
+	public String getMeetingId() {
+		return meetingId;
+	}
+
+	/**
+	 * @param meetingId the meetingId to set
+	 */
+	public void setMeetingId(String meetingId) {
+		this.meetingId = meetingId;
+	}
+	
+}
